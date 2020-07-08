889f0061871bfb19d4da77833bc1f78460d32398
vagrant <vagrant@localhost.localdomain>
2020-07-08 10:47:53 +0000

    [start]bookers-level2ä½
    rails routes
    
    git commit -m[start]bookers-level2ä½

---
+++ app/models/user.rb
@@ -0,0 +1,20 @@
+class User < ApplicationRecord
+  # Include default devise modules. Others available are:
+  # :confirmable, :lockable, :timeoutable, :trackable and :omniauthable
+  devise :database_authenticatable, :registerable,
+         :recoverable, :rememberable, :validatable
+
+  has_many :users, dependent: :destroy
+
+  validates :name, presence: true, uniqueness: true, length: {maximum: 20}
+
+  def email_required?
+    false
+  end
+
+  def email_changed?
+    false
+  end
+
+
+end
---
+++ config/routes.rb
@@ -0,0 +1,14 @@
+Rails.application.routes.draw do
+  
+  devise_for :users
+  # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
+
+  	root 'homes#top'
+
+  	get 'about' => 'homes#about'
+	
+	resources :books, only: [ :index, :show, :create, :edit, :destroy]
+
+	resources :users, only: [:show, :index]	
+
+end
